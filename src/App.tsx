74bd33a022042346c409514eb4098597620e6f37
Rufat Safiullin <safiullin_rr@devtrix.ru>
2024-04-05 19:56:34 +0500

    init: инициализация проекта

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './styles.css'
+import Image from './react-image.jpg'
+import Logo from './Tourist.svg'
+export const App = () => {
+  const num = "1"
+  return (
+    <>
+      <h1>Avito Tech dfsdfds</h1>
+
+      <img src={Image} alt="dsad" width={300} height={200} />
+      <img src={Logo} alt="dsad" width={300} />
+
+    </>
+  )
+}
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,13 @@
+const webpack = require("webpack");
+
+module.exports = {
+    mode: "development",
+    devtool: "cheap-module-source-map",
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                TOKEN: JSON.stringify(process.env.TOKEN),
+            },
+        }),
+    ],
+};
