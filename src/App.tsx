00260e4574aedbb95f33b08dcc09817e0becdcc6
Rufat Safiullin <safiullin_rr@devtrix.ru>
2024-04-05 20:08:06 +0500

    fix: починил husky

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,13 @@ import './styles.css'
 import Image from './react-image.jpg'
 import Logo from './Tourist.svg'
 export const App = () => {
-  const num = "1"
+  const num = '1'
   return (
     <>
       <h1>Avito Tech dfsdfds</h1>
 
       <img src={Image} alt="dsad" width={300} height={200} />
       <img src={Logo} alt="dsad" width={300} />
-
     </>
   )
 }
